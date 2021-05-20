31646df1db511214aa37d512555fcc21d71a445b
Sanele97 <84188072+Sanele97@users.noreply.github.com>
2021-05-20 19:00:03 +0200

    Add files via upload

---
+++ MyFirstProgram2021.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myfirstprogram2021;
+//IMPORTS CLASS LIBRARIES
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author MPISI SANELE
+ */
+//CLASS DEFINITION
+public class MyFirstProgram2021 {
+
+    /**
+     * @param args the command line arguments
+     */
+    //MAIN METHOD DEFINITION
+    public static void main(String[] args) {
+       //DISPLAY MESSAGE
+       JOptionPane.showMessageDialog(null,"Programming is fun.");
+    }
+    
+}
